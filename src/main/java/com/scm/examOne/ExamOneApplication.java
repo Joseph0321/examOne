182bcb29ca8c390261fca9478887b2aef10bbe7d
joseph <whale114@gmail.com>
2024-05-19 15:05:26 +0900

    example

---
+++ src/main/java/com/scm/examOne/ExamOneApplication.java
@@ -0,0 +1,13 @@
+package com.scm.examOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scm/examOne/ExamOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.examOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
